eb86f67138017099928d1bf4b00c29e3ccdb2ab0
Exppie <nr9691439@gmail.com>
2023-11-23 00:11:37 -0300

    commit

--- black_scholes.hpp
+++ black_scholes.hpp
@@ -47,8 +47,55 @@ double call_theta(const  double S, const double K, const double r, const double 
 
 // Call Rho
 double call_rho(const  double S, const double K, const double r, const double v, const double T) {
-    return K * T * exp(-r * T) * norm_cdf(d_j(2, S, K, r, v, T))
+    return K * T * exp(-r * T) * norm_cdf(d_j(2, S, K, r, v, T));
 }
 
 
 // Calculando o preço de uma Put com base em S, K, r, v, sigma e T
+double put_price(const double S, const double K, const double r, const double v, const double T) {
+    return -S * norm_cdf(-d_j(1, S, K, r, v, T)) + K * exp(-r * T) * norm_cdf (-d_j(2, K, r, v, T));
+}
+
+// Put Delta
+double put_delta(const double S, const double K, const double r, const double v, const double T) {
+    return norm_cdf(d_j(1, S, K, r, v, T)) - 1;
+}
+
+// Put Gamma
+double put_gamma(const double S, const double K, const double r, const double v, const double T) {
+    return call_gamma(1, S, K, r, v, T); // paridade put-call 
+}
+
+// Put Vega
+double put_vega(const double S, const double K, const double r, const double v, const double T){
+    return call_vega(S, K, r, v, T);
+}
+
+// Put Theta
+double put_theta(const double S, const double K, const double r, const double v, const double T) {
+    return - (S * norm_cdf(d_j(1, S, K, r, v, T)) * v) / (2 * sqrt(T)) + r * K * exp(-r * T) * norm_cdf(-d_j(2, S, K, r, v, T));
+}
+
+// Put Rho
+double put_rho(const double S, const double K, const double r, const double v, const double T) {
+    return -T * K * exp (-r * T) * norm_cdf(-d_j(2, S, K, r, v, T));
+}
+
+// Monte Carlo
+
+// Box Muller - gerar números gaussianos aleatórios para Monte Carlo
+double gaussian_box_muller() {
+    double x = 0.0;
+    double y = 0.0;
+    double euclid_sq = 0.0;
+
+do {
+    x = 2.0 * rand() / static_cast <double> (RAND_MAX) - 1;
+    y = 2.0 * rand() / static_cast <double> (RAND_MAX) - 1;
+    euclid_sq = x * x + y * y;
+}while (euclid_sq >= 1.0);
+
+return x * sqrt (-2 * log (euclid_sq) / euclid_sq);
+
+
+}
